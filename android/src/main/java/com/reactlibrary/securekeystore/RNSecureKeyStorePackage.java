68ac9c738b88f22954ece52f673675dbd338ada4
lenkavon <lenka.vondr@gmail.com>
2017-08-04 13:56:20 +0200

    RN v 47 - breaking change fix: remove unused registration of JS modules

--- android/src/main/java/com/reactlibrary/securekeystore/RNSecureKeyStorePackage.java
+++ android/src/main/java/com/reactlibrary/securekeystore/RNSecureKeyStorePackage.java
@@ -14,17 +14,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNSecureKeyStorePackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     return Arrays.<NativeModule>asList(new RNSecureKeyStoreModule(reactContext));
-  }
-
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
   }
 
   @Override
